997e51518587bf7b7475ae1e7c7838c909b4af9a
Anthony Broussard <anthony@chaione.com>
2011-08-18 20:57:56 -0500

    Basic chat

---
+++ demo.js
@@ -0,0 +1,41 @@
+var net = require("net");
+
+var clients = [];
+
+function Client(stream) {
+  this.stream = stream;
+	this.foo = "foo";
+}
+
+var server = net.createServer(function (stream) {
+  var client = new Client(stream);
+  clients.push(client);
+	console.log("new client: " + client.stream);
+	console.log("clients:" + clients);
+
+  stream.setTimeout(-1);
+  stream.setEncoding("utf8");
+
+  stream.addListener("connect", function () {
+		process.nextTick(function() {
+    	stream.write("Welcome!\n");
+  	});		
+	});
+	
+  stream.addListener("data", function (data) {
+		var broadcast = data.match(/\S+/)[0];
+		console.log("this is clients" + clients);
+		console.log('clients #0' + clients[0].stream);
+		for (var i = 0; i < clients.length; i = i + 1) {
+			var c = clients[i];
+			c.stream.write(broadcast+"\n");
+		}
+	});
+	
+	process.on('uncaughtException', function (err) {
+		console.log('WARNING! UNCAUGHT EXCEPTION: ' + err);
+	});
+		
+});
+
+server.listen(7000);
