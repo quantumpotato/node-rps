504ee33655ab84fe58e7dfea5a9f3ceb2d61d3a9
Anthony Broussard <anthony@chaione.com>
2011-08-18 21:41:05 -0500

    Can create and join games

--- demo.js
+++ demo.js
@@ -1,19 +1,48 @@
 var net = require("net");
 
 var clients = [];
+var games 	= [];
 
 function Client(stream) {
   this.stream = stream;
 	this.name = null;
 }
 
-function nameClient(client, data) {
+function nameClient(client, data, callback) {
 	client.name = data.match(/\S+/);
   clients.forEach(function(c) {
     if (c != client) {
-      c.stream.write(client.name + " has joined.\n");
+      c.stream.write(client.name + " has joined the server.\n");
     }
   });
+
+	callback(client);
+}
+
+function Game(client) {
+	this.players = [client];
+}
+
+function newGame(client) {
+	for (var i = 0; i < games.length; i = i + 1) {
+		var game = games[i];
+		if (game.players.length === 1) {
+			game.players[0].stream.write(client.name + " has joined your game.\n");
+			game.players.push(client);
+			return game;
+		}
+	};
+	
+	var aNewGame = new Game(client);
+	return aNewGame;
+}
+
+
+function findGameForClient(client) {
+	var game = newGame(client);
+	if (game.players.length === 1) {
+		games.push(game);
+	};	
 }
 
 function broadcastClientData(client, data) {
@@ -28,7 +57,7 @@ var server = net.createServer(function (stream) {
   var client = new Client(stream);
   clients.push(client);
 
-  stream.setTimeout(-1);
+  stream.setTimeout(0);
   stream.setEncoding("utf8");
 
   stream.addListener("connect", function () {
@@ -39,8 +68,9 @@ var server = net.createServer(function (stream) {
 	
 	stream.addListener("data", function (data) {
 	    if (client.name == null) {
-				nameClient(client, data);
-	      return;
+				process.nextTick(function() {
+					nameClient(client, data, findGameForClient)
+					});
 	    }
 	
 			broadcastClientData(client, data);
