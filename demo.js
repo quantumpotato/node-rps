7fe61d93af3eb7d136d2e905858008c6b026b21a
Anthony Broussard <anthony@chaione.com>
2011-08-22 20:13:06 -0500

    RPS instead of chat

--- demo.js
+++ demo.js
@@ -2,58 +2,224 @@ var net = require("net");
 
 var clients = [];
 var games 	= [];
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+eventEmitter.on("choiceValidated", processValidChoice);
+eventEmitter.on("availableGameFound",processAvailableGame);
+eventEmitter.on("playerNamed", findAvailableGame);
+eventEmitter.on("resultProcessed",processResult);
+
+function processResult(game, result) {
+	console.log("processing!");
+	
+	//Make this block be async with game.finish
+	if (result === "win") {
+		//Refactor to winner/loser message		
+		//Refactor parallel: winner/lose message (instead of game.players[#])
+		game.players[0].stream.write("Victory! You defeated " + game.players[1].name + "\n");
+		game.players[1].stream.write("Defeat! You lost to " + game.players[0].name + "\n");
+	} else if (result === "loss") {
+		game.players[1].stream.write("Victory! You defeated " + game.players[0].name + "\n");
+		game.players[0].stream.write("Defeat! You lost to " + game.players[1].name + "\n");			
+	} else if (result === "draw") {
+		game.players[0].stream.write("Draw game! Choose again wisely.\n");			
+		game.players[1].stream.write("Draw game! Choose again wisely.\n");
+	}
+
+	//use async library with above block
+	game.finish();
+}
+
+function Rock() {
+	this.evaluateChoice = function(choice) {
+		if (choice === "r") {
+			return "draw";
+		} else if (choice === "p") {
+			return "loss";
+		} else if (choice === "s") {
+			return "win";
+		}
+	}
+	
+	return this;
+}
+
+function Paper() {
+	this.evaluateChoice = function(choice) {
+		if (choice === "r") {
+			return "win";
+		} else if (choice === "p") {
+			return "draw";
+		} else if (choice === "s") {
+			return "loss";
+		}
+	}
+	
+	return this;
+}
+
+function Scissors() {
+	this.evaluateChoice = function(choice) {
+		if (choice === "r") {
+			return "loss";
+		} else if (choice === "p") {
+			return "win";
+		} else if (choice === "s") {
+			return "draw";
+		}
+	}
+	
+	return this;
+}
 
 function Client(stream) {
   this.stream = stream;
 	this.name = null;
-}
-
-function nameClient(client, data, callback) {
-	client.name = data.match(/\S+/);
-  clients.forEach(function(c) {
-    if (c != client) {
-      c.stream.write(client.name + " has joined the server.\n");
-    }
-  });
-
-	callback(client);
+	this.game = null;
+	this.choice = null;
+	this.getChoiceObject = function() {
+		if (this.choice === "r") {
+			return new Rock();
+		} else if (this.choice === "p") {
+			return new Paper();
+		} else if (this.choice === "s") {
+			return new Scissors();
+		}
+	}
+	
+	return this;
 }
 
 function Game(client) {
 	this.players = [client];
-}
-
-function newGame(client) {
-	for (var i = 0; i < games.length; i = i + 1) {
-		var game = games[i];
-		if (game.players.length === 1) {
-			game.players[0].stream.write(client.name + " has joined your game.\n");
-			game.players.push(client);
-			return game;
-		}
-	};
+	client.game = this;	
 	
-	var aNewGame = new Game(client);
-	return aNewGame;
+	this.available = function() {
+		if (this.players.length === 1) {
+			return true;
+		}
+		
+		return false;
+	}
+
+	this.finish = function() {
+		this.players[0].choice = null;
+		this.players[1].choice = null;
+	}
+	
+	this.isActive = function() {
+		if (this.players.length === 2) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	this.bothPlayersHaveDecided = function() {
+		console.log("player 0 choice: " + this.players[0].choice);
+		console.log("player 1 choice: " + this.players[1].choice);
+		if (this.players[0].choice != null && this.players[1].choice != null) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	this.evaluateChoices = function() {
+		var result = this.players[0].getChoiceObject().evaluateChoice(this.players[1].choice);
+		eventEmitter.emit("resultProcessed",this, result);
+	}
+	
+	this.startGame = function() {
+		process.nextTick(promptForMove(this.players[0]));
+		process.nextTick(promptForMove(this.players[1]));
+	}
+	
+	this.addSecondPlayer = function(newPlayer) {
+		this.players[0].stream.write(newPlayer.name + " has joined your game.\n"); 
+		newPlayer.stream.write("Joined game with " + this.players[0].name + "\n");
+		newPlayer.game = this;
+		this.players.push(newPlayer);
+		process.nextTick(this.startGame);
+	}
+	
+	this.announce = function(announcement) {
+		this.players[0].stream.write(announcement);
+		this.players[1].stream.write(announcement);
+	}
+	
+	return this;
 }
 
-
-function findGameForClient(client) {
-	var game = newGame(client);
-	if (game.players.length === 1) {
-		games.push(game);
-	};	
+function promptForMove(client) {
+	client.choice = null;
+	client.stream.write("Enter r, p or s \n");
 }
 
-function broadcastClientData(client, data) {
-	clients.forEach(function(c) {
-    if (c != client) {
-      c.stream.write(client.name + ": " + data);
-    }
+function nameClient(client, data) {
+	client.name = data.match(/\S+/);
+  clients.forEach(function(c) {
+    process.nextTick(function() {
+			if (c != client) {
+	      c.stream.write(client.name + " has joined the server.\n");
+	    }
+		})
   });
+
+	eventEmitter.emit("playerNamed", client);
 }
 
-var server = net.createServer(function (stream) {
+function findAvailableGame(player) {
+	
+	//Todo: make this asynchronously awesome?
+	var availableGame = null;
+	for (var i = 0; i < games.length; i++) {
+		var game = games[i];
+		if (game.available()) {
+			availableGame = game;
+		};
+	}
+	
+	eventEmitter.emit("availableGameFound", player, availableGame);
+}
+
+function processAvailableGame(client, game) {
+	if (game) {
+		game.addSecondPlayer(client);
+	} else {
+		game = new Game(client);
+		games.push(game);
+	}
+}
+
+function validateRPSChoice(player, data) {
+	var choice = data.match(/\S+/)[0];
+	console.log(player.name + " chose " + choice);	
+	if (choice === "r" || choice === "p" || choice === "s") {
+		eventEmitter.emit("choiceValidated", player, choice);
+	} else {
+		process.nextTick(function(){
+			player.stream.write("Please choose r, p or s. \n");
+		});
+	}
+}
+ 
+function processValidChoice(player, choice) {
+	player.choice = choice;		
+	player.stream.write("You chose " + choice + "\n");
+	var game = player.game;
+	if (game.isActive() && game.bothPlayersHaveDecided()) {
+		process.nextTick(game.evaluateChoices());
+	}
+}
+
+function processInput(player, data) {
+	//Leaving process input here because we may not want to do other actions besides validateRPSChoice
+	process.nextTick(validateRPSChoice(player, data));	
+}
+
+var server = net.createServe r(function (stream) {
   var client = new Client(stream);
   clients.push(client);
 
@@ -61,19 +227,22 @@ var server = net.createServer(function (stream) {
   stream.setEncoding("utf8");
 
   stream.addListener("connect", function () {
-		process.nextTick(function() {
     	stream.write("Welcome! Please enter your name. \n");
-  	});		
 	});
 	
 	stream.addListener("data", function (data) {
-	    if (client.name == null) {
-				process.nextTick(function() {
-					nameClient(client, data, findGameForClient)
-					});
-	    }	    
-	  });	
-		
+    if (client.name === null) {
+				nameClient(client, data);
+				return;
+    }
+
+		processInput(client, data);    
+	 });			
+	
+	process.on('uncaughtException', function (err) {
+		console.log('WARNING! UNCAUGHT EXCEPTION: ' + err);
+	});
+	
 });
 
 server.listen(7000);
